2add81d7038ef27e6b5607775c9b992020f62b36
Chugunov Roman <chugunovrv@lad24.ru>
2022-04-10 23:23:57 +0300

    fix: Update code

--- src/lua_extensions.cpp
+++ src/lua_extensions.cpp
@@ -1,9 +1,13 @@
 #include "lua_extensions.h"
 
+static const luaL_Reg R[] = {{NULL, NULL}};
+
 LUALIB_API int luaopen_lua_extensions(lua_State *L)
 {
   open_table(L);
   open_string(L);
+
+  luaL_register(L, LUA_EXTENSIONSLIBNAME, R);
 
   return 1;
 }
